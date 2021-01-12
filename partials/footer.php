9f74dc8340d196cca1d61d7f537c3829f5973e0c
victornavarrete <victornavarrete@live.cl>
2021-01-12 01:13:55 -0300

    codigo base

---
+++ partials/footer.php
@@ -0,0 +1,15 @@
+<footer> <small>2021</small> </footer>
+
+<script src="/assets/js/vendor/modernizr-3.11.2.min.js"></script>
+<script src="/assets/js/plugins.js"></script>
+<script src="/assets/js/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+<script>
+window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+ga('create', '<?php echo CONFIG["ga_key"] ?>', 'auto'); 
+ga('set', 'anonymizeIp', true);
+ga('set', 'transport', 'beacon');
+ga('send', 'pageview');
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async></script>
---
+++ views/home.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  
+  <title> <?php echo CONFIG['name'] ?> / Home </title>
+  <meta name="description" content="">
+  <?php load_partial('head'); ?>  
+</head>
+
+<body>
+
+  <!-- Add your site or application content here -->
+  <h1>Hello world!</h1>
+  <p> This is the <strong>Home</strong>.</p>  
+
+  <?php load_partial('footer'); ?> 
+</body>
+
+</html>
